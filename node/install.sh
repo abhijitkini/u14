487b922320a95c6c1bcad30a0589c1270cef94ae
Abhijit Kini <abhijit@shippable.com>
2016-09-22 11:08:45 -0700

    Updating node install to 6.x
    - https://github.com/creationix/nvm#install-script for nvm
    - https://github.com/nodesource/distributions#manual-installation for node

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "================= Installing NVM ==================="
-curl https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 
 # Set NVM_DIR so the installations go to the right place
 export NVM_DIR="/root/.nvm"
@@ -9,7 +9,9 @@ export NVM_DIR="/root/.nvm"
 # add source of nvm to .bashrc - allows user to use nvm as a command
 echo "source ~/.nvm/nvm.sh" >> $HOME/.bashrc
 
-echo "================= Installing latest nodejs ==================="
-add-apt-repository -y ppa:chris-lea/node.js
+echo "================= Installing nodejs 6.x ==================="
+curl --silent https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+echo "deb https://deb.nodesource.com/node_6.x trusty main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb-src https://deb.nodesource.com/node_6.x trusty main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 apt-get update
 apt-get install -y nodejs
